2a64006f1810f71717e51b7815709bf6bdeed696
tyler <dtctorrero@icloud.com>
2022-07-10 12:55:12 -0700

    add user signup backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,7 +7,7 @@ const usersRouter = require('./users');
 
 router.use(restoreUser);
 router.use('/session', sessionRouter);
-router.use('users', usersRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
